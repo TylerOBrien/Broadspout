1e1fe9c7ddfbff715853cd61ec3f398531c0ed90
Tyler O'Brien <hello@tylerobrien.com>
2024-02-21 13:58:18 -0500

    Add bouncing image logic

--- system/TweenAnimation/animations.ts
+++ system/TweenAnimation/animations.ts
@@ -1,4 +1,10 @@
 /**
+ * System Imports
+*/
+
+import { AABB } from '@system/Geometry';
+
+/**
  * Relative Imports
 */
 
@@ -8,9 +14,34 @@ import { BouncingImageItem } from './types';
  * Public Functions
 */
 
+/**
+ * @param {BouncingImageItem<ImageTy>} item
+ * @param {AABB} within
+ * @param {number} delta
+ *
+ * @return {void}
+ */
 export function TweenAnimationBounce<ImageTy>(
     item: BouncingImageItem<ImageTy>,
-    delta: number = 1)
+    within: AABB,
+    delta: number = 1): void
 {
-    //
+    item.bounds.x += item.velocity.translate.x * delta;
+    item.bounds.y += item.velocity.translate.y * delta;
+
+    if (item.velocity.translate.x > 0 && (item.bounds.x + item.bounds.hw) > within.w) {
+        item.bounds.x = item.bounds.hw;
+        item.velocity.translate.x = -item.velocity.translate.x;
+    } else if (item.velocity.translate.x < 0 && item.bounds.x < 0) {
+        item.bounds.x = 0;
+        item.velocity.translate.x = -item.velocity.translate.x;
+    }
+
+    if (item.velocity.translate.y > 0 && (item.bounds.y + item.bounds.hh) > within.h) {
+        item.bounds.y = item.bounds.hh;
+        item.velocity.translate.y = -item.velocity.translate.y;
+    } else if (item.velocity.translate.y < 0 && item.bounds.y < 0) {
+        item.bounds.y = 0;
+        item.velocity.translate.y = -item.velocity.translate.y;
+    }
 }
