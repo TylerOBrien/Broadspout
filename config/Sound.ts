4a21a070a0a80e6b28752a0167858d7cd8c7f057
Tyler O'Brien <hello@tylerobrien.com>
2024-02-22 11:39:42 -0500

    Add cooldownEnabled var

--- config/Sound.ts
+++ config/Sound.ts
@@ -12,6 +12,7 @@ export const SoundConfig = {
     sounds: {
         get uri() { return Config('sound-uri', '/json/sounds.json'); },
     },
+    get cooldownEnabled() { return true; },
     get cooldownResponseEnabled() { return true; },
     get maxFetchAttempts() { return 5; },
     get defaultDelayAfterPlayback() { return 33; },
