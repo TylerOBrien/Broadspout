56e960074d62fe9ca8c1c3a48047fb3fec7abb30
Tyler O'Brien <hello@tylerobrien.com>
2024-03-11 11:38:41 -0400

    Add ImageConfig

---
+++ config/Image.ts
@@ -0,0 +1,15 @@
+/**
+ * System
+*/
+
+import { DurationType, DurationTuple } from '@system/Chrono';
+
+/**
+ * Config
+*/
+
+export const ImageConfig = {
+    default: {
+        TTL: [1, DurationType.Hours] as DurationTuple,
+    },
+};
