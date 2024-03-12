91fc95aafbf874be8f2720a7f3b0752da3411889
Tyler O'Brien <hello@tylerobrien.com>
2024-03-12 10:10:05 -0400

    Add Units types to Geom

--- system/Geometry/types.ts
+++ system/Geometry/types.ts
@@ -4,10 +4,22 @@ export interface Point
     y: number;
 };
 
+export interface PointUnits
+{
+    x: 'px' | 'vw' | '%';
+    y: 'px' | 'vh' | '%';
+};
+
 export interface Extent
 {
     w: number;
     h: number;
+};
+
+export interface ExtentUnits
+{
+    w: 'px' | 'vw' | '%';
+    h: 'px' | 'vh' | '%';
 };
 
 export interface AABB extends Point, Extent
@@ -15,3 +27,6 @@ export interface AABB extends Point, Extent
     hw: number;
     hh: number;
 };
+
+export interface AABBUnits extends PointUnits, ExtentUnits
+{};
