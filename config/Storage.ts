b77f28bccaa834a41ba9a50f5da25cd17de8a8a0
Tyler O'Brien <hello@tylerobrien.com>
2024-03-13 10:16:39 -0400

    Adjust whitespace

--- config/Storage.ts
+++ config/Storage.ts
@@ -3,8 +3,8 @@ import { StorageMode } from '@system/Storage';
 export const StorageConfig = {
     remote: {
         get protocol() { return 'http'; },
-        get host() { return 'localhost'; },
-        get port() { return 80; },
+        get host()     { return 'localhost'; },
+        get port()     { return 80; },
     },
     get defaultMode() { return StorageMode.Local; }
 };
