1be8398fc4a72d871fc43aea74b42c83d0e9f534
Tyler O'Brien <hello@tylerobrien.com>
2024-03-13 10:21:15 -0400

    Add url to StorageConfig

--- config/Storage.ts
+++ config/Storage.ts
@@ -14,5 +14,6 @@ export const StorageConfig = {
         get protocol() { return Env('storage-protocol', 'http'); },
         get host()     { return Env('storage-host', 'localhost'); },
         get port()     { return Env('storage-port', '80'); },
+        get url()      { return `${ Env('storage-protocol', 'http') }://${ Env('storage-host', 'localhost') }:${ Env('storage-port', '80') }`; },
     },
 };
