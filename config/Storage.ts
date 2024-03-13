6e4fe8731500ee24352b61c35c47ff10a9f60bd5
Tyler O'Brien <hello@tylerobrien.com>
2024-03-13 10:18:02 -0400

    Use Env to fetch config values

--- config/Storage.ts
+++ config/Storage.ts
@@ -2,6 +2,7 @@
  * System
 */
 
+import { Env } from '@system/Env';
 import { StorageMode } from '@system/Storage';
 
 /**
@@ -10,9 +11,9 @@ import { StorageMode } from '@system/Storage';
 
 export const StorageConfig = {
     remote: {
-        get protocol() { return 'http'; },
-        get host()     { return 'localhost'; },
-        get port()     { return 80; },
+        get protocol() { return Env('storage-protocol', 'http'); },
+        get host()     { return Env('storage-host', 'localhost'); },
+        get port()     { return Env('storage-port', '80'); },
     },
     get defaultMode() { return StorageMode.Local; }
 };
